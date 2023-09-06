ac55424df9c08eda8c22466a05e5590c79fcec66
Shawn-Cummins <shawn.cummins@dcsg.com>
2023-09-06 15:24:48 -0400

    PTR-6562 add 2 more vms np (#4)
    * PTR-6562 add 2 more vms np
    
    * fix vm name

---
+++ infra/nonprod/vm_02.tf
@@ -0,0 +1,17 @@
+locals {
+  vm02_name = "vm-vault-np-02"
+  vm02_size = "Standard_D4_v3"
+  vm02_avail_zone = "2"
+}
+
+module "vm02" {
+  source               = "../modules/vm"
+  vm_name = local.vm02_name
+  resource_group = module.rg.resource_group_name
+  subnet_id = local.vm_subnet_id
+  location = local.location
+  vm_size = local.vm02_size
+  avail_zone = local.vm02_avail_zone
+  disk_type = "StandardSSD_LRS"
+  os_image = "/subscriptions/c61b06f4-16b9-4f39-b832-e1012463b0cb/resourceGroups/RG-VRA8-POC/providers/Microsoft.Compute/galleries/DSG.90541.ImageGallery/images/IMG-Ubuntu-2204/versions/0.0.1"
+}
