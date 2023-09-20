304159150ff158597810901a4bac3146062f6425
Khiry Powell <Khiry.Powell@dcsg.com>
2023-09-20 15:30:54 -0400

    enable healthcheck and persistence

--- infra/modules/load_balancer/main.tf
+++ infra/modules/load_balancer/main.tf
@@ -31,3 +31,28 @@ resource "azurerm_lb_backend_address_pool" "vault_backend" {
   loadbalancer_id = azurerm_lb.vault_lb.id
   name            = "vault-backendaddresspool"
 }
+
+resource "azurerm_lb_rule" "vault_webrule" {
+  loadbalancer_id                = azurerm_lb.vault_lb.id
+  name                           = "vault-webrule"
+  protocol                       = "Tcp"
+  frontend_port                  = 443
+  backend_port                   = 8200
+  frontend_ip_configuration_name = "vault-frontend"
+  backend_address_pool_ids = [azurerm_lb_backend_address_pool.vault_backend.id]
+  probe_id = azurerm_lb_probe.health_probe.id
+  enable_floating_ip = false
+  idle_timeout_in_minutes = 4
+  disable_outbound_snat = false
+}
+
+resource "azurerm_lb_probe" "health_probe" {
+  loadbalancer_id = azurerm_lb.vault_lb.id
+  name            = "vault-health-probe"
+  port            = 8200
+  protocol        = "Http"
+  probe_threshold = 1
+  request_path = "/sys/health"
+  interval_in_seconds = 15
+  number_of_probes = 2
+}
