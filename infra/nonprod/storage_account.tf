747c516f3a413ff6dc514f554e9e9603fda19e56
KhiryPowell <90347151+KhiryPowell@users.noreply.github.com>
2023-11-14 11:07:39 -0500

    created raft backup container (#23)
    * created raft backup container
    
    * adding storage account name output

--- infra/nonprod/storage_account.tf
+++ infra/nonprod/storage_account.tf
@@ -1,5 +1,3 @@
-
-
 module "sa" {
   source              = "../modules/storage_account"
   name                = "stdsgvaultnpue"
@@ -9,4 +7,10 @@ module "sa" {
   firewall_subnet_ids = [local.vm_subnet_id]
   firewall_tags       = ["CLIENT-VPN", "GITHUB-ACTIONS"]
   tags                = local.tags
+}
+
+resource "azurerm_storage_container" "raftbackup" {
+  name                  = "raftbackup"
+  storage_account_name  = module.sa.name
+  container_access_type = "private"
 }
